1aa1f9cd75f2078d7615980f75906efcbaeab34f
UmutsUzun <sarperuzun1@gmail.com>
2025-03-20 19:29:24 +0000

    Product quantity change buttons

--- client/src/components/cartTotals/CartTotals.jsx
+++ client/src/components/cartTotals/CartTotals.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 import {ClearOutlined, PlusCircleOutlined , MinusCircleOutlined} from "@ant-design/icons"
 import {useSelector} from "react-redux";
 import { useDispatch } from 'react-redux';
-import { deleteCart } from '../../redux/cartSlice';
+import { deleteCart , increase,decrease } from '../../redux/cartSlice';
+
 
 const Cart_Totals = () => {
   const cart = useSelector((state) => state.cart);
@@ -12,7 +13,8 @@ const Cart_Totals = () => {
     <div className='cart h-full max-h-[calc(100vh_-_90px)] flex flex-col '>
       <h2 className='bg-blue-600 text-center py-4 text-white'>Items in the basket</h2>
       <ul className='cart_items px-2 flex flex-col gap-y-3 py-2 overflow-y-auto'>
-        {cart.cartItems.map((item)=>(
+        {cart.cartItems.length > 0 
+        ? cart.cartItems.map((item)=>(
                   <li className='cart-item flex justify-between' key={item._id}>
                   <div className='flex items-center'>
                     <img 
@@ -26,19 +28,33 @@ const Cart_Totals = () => {
                       <span>{item.price} x {item.quantity} </span>
                     </div>            
                   </div>
-                  <div className='flex items-center gap-x-1'>
+                  <div className='flex items-center'>
                   <Button type="primary" size="small"
                   className='w-full flex items-center
                   justify-center !rounded-full'
-                  icon ={<PlusCircleOutlined />}/>
-                  <span className='font-bold '>{item.quantity}</span>
+                  icon ={<PlusCircleOutlined />}
+                  onClick={()=> dispatch(increase(item))}
+                  />
+                  
+                  <span className='font-bold w-6 inline-block text-center '>{item.quantity}</span>
                   <Button type="primary" size="small"
                   className='w-full  flex items-center
                   justify-center !rounded-full'
-                  icon ={<MinusCircleOutlined />}/>
+                  icon ={<MinusCircleOutlined />}
+                  onClick={()=> {
+                    if (item.quantity ===1){
+                      if(window.confirm("Delete the product?")){
+                        dispatch(decrease(item));
+                      }
+                    }
+                    if(item.quantity > 1) {
+                      dispatch(decrease(item));
+                    }
+                  } }
+                  />
                   </div>
                 </li>
-        ))}
+        )) : "Basket is empty..."}
       </ul>
       <div className="cart-totals mt-auto">
         <div className='border-t '>
